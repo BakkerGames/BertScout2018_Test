d8e88a8b70c23ad29bdac596a3e59c3dec4a0cd7
ELYSIAN\chime <scott@bakkergames.com>
2018-01-18 20:44:10 -0500

    Have launching match working

--- app/src/main/java/com/example/chime/bertscout2018_test/MatchActivity.java
+++ app/src/main/java/com/example/chime/bertscout2018_test/MatchActivity.java
@@ -1,7 +1,9 @@
 package com.example.chime.bertscout2018_test;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MatchActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MatchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_match);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = findViewById(R.id.textViewMessage);
+        textView.setText(message);
     }
 }
